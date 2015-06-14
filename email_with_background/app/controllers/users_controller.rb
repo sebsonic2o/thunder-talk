f47473b63ad4429ef065f1ffaf4d7b23f67d7ff1
banned4hax & Tapley Stephenson <pair+banned4hax.tapley@devbootcamp.com>
2015-06-13 19:02:31 -0700

    Add initial email delivery app with background

---
+++ email_with_background/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    UserMailer.welcome_email(@user).deliver
+
+    redirect_to '/'
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email)
+    end
+end
